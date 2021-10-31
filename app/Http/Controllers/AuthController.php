4d9b5bf6f975fd114c3e3ce29b67897945c828e8
beutyi <iklimaabeauty@gmail.com>
2021-10-31 13:02:33 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,79 +16,52 @@ class AuthController extends Controller
             "Class" => "XII RPL2"
         ];
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+        $user = User::where('email', $fields['email'])->first();
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email'=> 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('tokenku')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message' => 'Logged out'
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +16,33 @@ use App\Http\Controllers\AuthorController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/me', [AuthController::class, 'me']);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+  return $request->user();
 });
 
-Route::resource('/books', BookController::class);
-Route::resource('/authors', AuthorController::class);
-Route::get('/me', [AuthController::class, 'me']);
+//  login register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+/* Authors Books*/
+// public
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/books/search/{title}', [BookController::class, 'search']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::get('/authors/search/{name}', [AuthorController::class, 'search']);
+
+// protected
+Route::middleware(['auth:sanctum'])->group(function () {
+  Route::post('/books', [BookController::class, 'store']);
+  Route::put('/books/{id}', [BookController::class, 'update']);
+  Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+  Route::post('/authors', [AuthorController::class, 'store']);
+  Route::put('/authors/{id}', [AuthorController::class, 'update']);
+  Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+  Route::post('/logout', [AuthController::class, 'logout']);
+});
